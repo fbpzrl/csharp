48c9bca02f1ca3366bd7ba22f77e20a70f086e81
Fabio Piazzarollo <fbpzrl@gmail.com>
2021-11-13 20:13:05 -0300

    Estruturas Sequenciais - Exercícios 1 e 2

---
+++ csharp/Csharp/3 - Lógica de Programação com Csharp/Estrutura Sequencial/Exercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Logica.EstruturaSequencial
+{
+    class Exercicio1
+    {
+        public Exercicio1() 
+        {
+            int valor1, valor2;
+
+            Console.Write("Digite dois números inteiros: ");
+            string[] vect = Console.ReadLine().Split(' '); //captura vários inputs separados por espaço
+            valor1 = int.Parse(vect[0]);
+            valor2 = int.Parse(vect[1]);
+
+            Console.WriteLine("SOMA = " + (valor1 + valor2));
+        }
+    }
+}
---
+++ csharp/Csharp/3 - Lógica de Programação com Csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Logica.EstruturaSequencial;
+
+namespace Logica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Exercicio1();
+            //new Exercicio2();
+        }
+    }
+}

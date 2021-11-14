6f8ee068e717f7f26f8e4466c3c878bca8ab498a
Fabio Piazzarollo <fbpzrl@gmail.com>
2021-11-14 15:32:21 -0300

    Adicionando enunciado dos exercícios

---
+++ csharp/Csharp/3 - Lógica de Programação com Csharp/1 - Estrutura Sequencial/Exercicio2.cs
@@ -0,0 +1,24 @@
+﻿//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+//casas decimais conforme exemplos
+
+using System;
+using System.Globalization;
+
+namespace Logica.EstruturaSequencial
+{
+    class Exercicio2
+    {
+        public Exercicio2()
+        {
+            double raio, area, pi = 3.14159;
+
+            Console.Write("Digite o raio do círculo: ");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //área do círculo: pi * (raio * raio)
+            area = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A = " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}

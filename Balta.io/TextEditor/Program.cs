39473b23c1add2475943c6de4d75d70d5ad8d14a
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-02-04 14:49:30 -0300

    TextEditor

---
+++ Balta.io/TextEditor/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short opcao = short.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            switch (opcao)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o caminho do arquivo:");
+            string diretorio = Console.ReadLine();
+
+            using(var arquivo = new StreamReader(diretorio))
+            {
+                string texto = arquivo.ReadToEnd();
+                Console.WriteLine(texto);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC) para sair)");
+            string texto = "";
+
+            do
+            {
+                texto += Console.ReadLine();
+                texto += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(texto);
+        }
+
+        static void Salvar(string texto)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var diretorio = Console.ReadLine();
+
+            using (var arquivo = new StreamWriter(diretorio))
+            {
+                arquivo.Write(texto);
+            }
+
+            Console.WriteLine($"Arquivo {diretorio} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}

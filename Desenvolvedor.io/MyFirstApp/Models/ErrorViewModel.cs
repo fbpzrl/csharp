9f3a28d5c2d67ab47a074d73d72222e19b154bf4
Fabio Piazzarollo <71791318+fbpzrl@users.noreply.github.com>
2022-03-27 15:20:37 -0300

    Delete Desenvolvedor.io directory

--- Desenvolvedor.io/MyFirstApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

7fe6f92a39eddb494d0d59fc0e9d4d27746789fe
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-04-12 21:02:19 -0300

    Add modulos and categoria routes

---
+++ Desenvolvedor.io/MyMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMVCDemo.Models;
+using System.Diagnostics;
+
+namespace MyMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string id, string categorias)
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
